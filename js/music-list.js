7f3c15d8a2efe7194976e6ea971ea65d1bdc24b0
Guilherme <guilhermefrm2208@gmail.com>
2025-09-08 00:56:55 -0300

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+{
+    name: "Os Anjos Cantam",
+    artist: "Jorge & Mateus",
+    img: "music-1",
+    src: "music-1"
+},
+{
+    name: "Amo Noite e Dia",
+    artist: "Jorge & Mateus",
+    img: "music-2",
+    src: "music-2"
+},
+{
+    name: "Te Assumi Pro Brasil",
+    artist: "Matheus & Kauan",
+    img: "music-3",
+    src: "music-3"
+},
+{
+    name: "Que Sorte A Nossa",
+    artist: "Matheus & Kauan",
+    img: "music-4",
+    src: "music-4"
+},
+{
+    name: "Entregador de Flor",
+    artist: "Diego & Victor Hugo",
+    img: "music-5",
+    src: "music-5"
+},
+{
+    name: "Flor e o Beija-flor",
+    artist: "Marília Mendonça",
+    img: "music-6",
+    src: "music-6"
+},
+
+];
